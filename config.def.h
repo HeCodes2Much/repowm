1d204af77f85a6d2a0b255251e58fd653613e198
The-Repo-Club <wayne6324@gmail.com>
2021-04-11 19:29:02 +0100

    fixed bar height

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int useinstabar 		= 1;  		/* 0 means don't use instabar script */
-static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 28;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[] = {
     "Misc Termsyn:size=12",
     "Siji:size=14",
