f30ef892cd7b8db8775d17b8dc92720be67ff5f6
The-Repo-Club <wayne6324@gmail.com>
2021-04-12 16:27:27 +0100

    set status

--- instawm.c
+++ instawm.c
@@ -3100,12 +3100,17 @@ main(int argc, char *argv[])
 {
 	if (argc == 2 && !strcmp("-v", argv[1]))
 		die("instawm-"VERSION);
-	else if (argc != 1)
+	else if (argc != 1 && strcmp("-s", argv[1]))
 		die("usage: instawm [-v]");
 	if (!setlocale(LC_CTYPE, "") || !XSupportsLocale())
 		fputs("warning: no locale support\n", stderr);
 	if (!(dpy = XOpenDisplay(NULL)))
 		die("instawm: cannot open display");
+	if (argc > 1 && !strcmp("-s", argv[1])) {
+		XStoreName(dpy, RootWindow(dpy, DefaultScreen(dpy)), argv[2]);
+		XCloseDisplay(dpy);
+		return 0;
+	}
 	if (!(xcon = XGetXCBConnection(dpy)))
 		die("instawm: cannot get xcb connection\n");
 	checkotherwm();
