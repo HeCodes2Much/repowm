bd745bfa41bee137ebda9715e4de88137952bb55
XDream8 <62709801+XDream8@users.noreply.github.com>
2021-04-13 13:54:24 +0300

    remove not necessary patchs

--- config.def.h
+++ config.def.h
@@ -11,11 +11,10 @@ static const unsigned int gappov    			= 30;       /* vert outer gap between win
 static const unsigned int systraypinning 		= 0;   		/* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft 		= 0;   		/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing 		= 2;   		/* systray spacing */
-static const double defaultopacity  			= 1;		/* 0 being full transparent 1 being non-transparent */
 static const int swallowfloating    			= 0;        /* 1 means swallow floating windows by default */
 static const int smartgaps          			= 0;        /* 1 means no outer gap when there is only one window */
 static const int systraypinningfailfirst 		= 1;   		/* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        			= 0;     	/* 0 means no systray */
+static const int showsystray        			= 1;     	/* 0 means no systray */
 static const int showbar            			= 1;        /* 0 means no bar */
 static const int topbar             			= 1;        /* 0 means bottom bar */
 static const int useinstabar 					= 1;  		/* 0 means don't use instabar script */
@@ -37,19 +36,11 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_green[] 		= "#52E167";
 
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
-
 static const char *colors[][4] = {
 	/*               	fg              	bg              	border 	           	 float*/
 	[SchemeNorm] 		= { col_gray3, 		col_gray1, 			col_gray2, 			col_green },
 	[SchemeInv]  		= { col_gray1, 		col_gray3, 			col_gray2, 			col_green },
 	[SchemeSel]  		= { col_gray4, 		col_cyan,  			col_cyan, 			col_green },
-};
-
-static const unsigned int alphas[][3] = {
-	[SchemeNorm] 		= { OPAQUE, 		baralpha, 			borderalpha },
-	[SchemeSel]  		= { OPAQUE, 		baralpha, 			borderalpha },
 };
 
 static const char *const autostart[] = {
