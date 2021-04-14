f4ab5eb8bd7ada850def67be22db0ac0e214309a
The-Repo-Club <wayne6324@gmail.com>
2021-04-14 17:45:43 +0100

    Fix Font

--- instawm.c
+++ instawm.c
@@ -1064,12 +1064,13 @@ drawbar(Monitor *m)
 	}
 	w = blw = TEXTW(m->ltsymbol);
 	drw_setscheme(drw, scheme[SchemeNorm]);
-	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
+	x = drw_text(drw, x, 0, w, bh, (w - TEXTW(m->ltsymbol)) * 0.5 + 10, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - stw - x) > bh) {
 		if (m->sel) {
+			int mid = (m->ww - TEXTW(m->sel->name)) / 2 - x;
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+			drw_text(drw, x, 0, w, bh, mid, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
@@ -1084,6 +1085,7 @@ drawbar(Monitor *m)
 		}
 		// display help message if no application is opened
 		if (!selmon->clients) {
+			int mid = (m->ww - TEXTW(m->sel->name)) / 2 - x;
 			int titlewidth =
 				TEXTW(" Press alt + space to launch an application") <
 						selmon->btw
@@ -1093,7 +1095,7 @@ drawbar(Monitor *m)
 			drw_text(
 				drw,
 				x + bh + ((selmon->btw - bh) - titlewidth + 1) / 2,
-				0, titlewidth, bh, 0,
+				0, titlewidth, bh, mid,
 				" Press alt + space to launch an application", 0);
 		}
 	}
