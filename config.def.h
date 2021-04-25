ec5eeb1453cd08546f012aa5f2f4574c236ccb0b
XDream8 <62709801+XDream8@users.noreply.github.com>
2021-04-26 00:51:11 +0300

    increase borderpx

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  		    = 1;        	    /* border pixel of windows */
+static const unsigned int borderpx  		    = 3;        	    /* border pixel of windows */
 static const unsigned int snap      		    = 32;       	    /* snap pixel */
 static const unsigned int gappih    		    = 10;       	    /* horiz inner gap between windows */
 static const unsigned int gappiv    		    = 10;       	    /* vert inner gap between windows */
